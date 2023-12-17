bbd5c4ff2662f22fd2b35d500651f16cbc813eb8
Artem <adelelain@gmail.com>
2023-12-17 14:06:46 +0300

    Initial Commit

---
+++ sparrotsprint7/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  sparrotsprint7
+//
+//  Created by Artem Dragunov on 17.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Button(action: {
+            
+        }, label: {
+            Label("Sign In", systemImage: "arrowshape.left.fill")
+        }).buttonStyle(.borderedProminent)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sparrotsprint7/sparrotsprint7App.swift
@@ -0,0 +1,17 @@
+//
+//  sparrotsprint7App.swift
+//  sparrotsprint7
+//
+//  Created by Artem Dragunov on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct sparrotsprint7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
